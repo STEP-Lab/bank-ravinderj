ebd9cd13a3c45295ec0054dedd7e8f8d4ad9488f
ravinder <jajoriaravinder99@gmail.com>
2018-03-19 18:55:14 +0530

    Added functionality to get account number of an account.

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,13 +1,26 @@
 import com.step.bank.Account.Account;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTest {
+
+  private Account account;
+
+  @Before
+  public void setUp() {
+    account = new Account("1234", 1000);
+  }
+
   @Test
   public void checkBalance() {
-    Account account = new Account("1234", 1000);
     assertThat(account.getBalance(), is(1000));
+  }
+
+  @Test
+  public void getAccountNumber() {
+    assertThat(account.getAccountNumber(), is("1234"));
   }
 }
