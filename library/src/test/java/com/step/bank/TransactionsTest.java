9d9bcee880ca5634f63aa88a2fc8bfe7e524afd9
ravinder <jajoriaravinder99@gmail.com>
2018-03-26 17:24:04 +0530

    Changed test assertion from hasItem to hasItems.

--- library/src/test/java/com/step/bank/TransactionsTest.java
+++ library/src/test/java/com/step/bank/TransactionsTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.Date;
 
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertThat;
 
 public class TransactionsTest {
@@ -32,8 +33,9 @@ public class TransactionsTest {
   @Test
   public void mustRecordBothCreditAndDebitTransactions() {
     transactions.debit(1000, "1234-5678");
-    assertThat(transactions.list,hasItem(new DebitTransaction(new Date(),1000,"1234-5678")));
     transactions.credit(1000, "1234-5678");
-    assertThat(transactions.list,hasItem(new CreditTransaction(new Date(),1000,"1234-5678")));
+    DebitTransaction debitTransaction = new DebitTransaction(new Date(),1000,"1234-5678");
+    CreditTransaction creditTransaction = new CreditTransaction(new Date(),1000,"1234-5678");
+    assertThat(transactions.list,hasItems(debitTransaction, creditTransaction));
   }
 }
